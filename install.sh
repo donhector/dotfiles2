711d98a988a9b7bbf98bf8a4aba7cc2c09eea690
Hector Molina <donhector@darkenv.com>
2021-10-18 16:54:44 +0200

    Updates

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -eux
 
 # Symlink certain files/directories in the user's home.
 link () {
-  for file in $( ls -A | grep -vE '\.git$|\.gitignore|\.gitmodules|.*.md' ) ; do
+  for file in $( \ls -A | grep -vE '\.git$|\.gitignore|\.gitmodules|.*.md' ) ; do
     # We force the creation in case the files already existed
     ln -sfnv "${PWD}/${file}" "${HOME}"
   done
@@ -21,35 +21,32 @@ if [ -n "${WSL_DISTRO_NAME}" ]; then
     get_win_var () { cmd.exe /c "echo ${1}" 2> /dev/null | sed -e 's/\r//g'; }
     
     # Symlink vscode settings
-    VSCODE_USER_FOLDER=$(wslpath $(get_win_var %APPDATA%/Code/User/))
-
+    VSCODE_USER_FOLDER=$(wslpath $(get_win_var %APPDATA%/Code/User))
     if [ -d ${VSCODE_USER_FOLDER} ]; then 
         echo "Symlinking VSCode user files..."
-        ln -sv ${PWD}/vscode/settings.json ${VSCODE_USER_FOLDER}/settings.json
-        ln -sv ${PWD}/vscode/keybindings.json ${VSCODE_USER_FOLDER}/keybindings.json
+        ln -sfnv "${PWD}/.config/Code/User/settings.json" "${VSCODE_USER_FOLDER}/settings.json"
+        ln -sfnv "${PWD}/.config/Code/User/keybindings.json" "${VSCODE_USER_FOLDER}/keybindings.json"
+
+        cat "${PWD}/.config/Code/User/extensions.txt | while read extension"
+        do
+          code --install-extension ${extension} --force
+        done
     else
         echo "No VSCode installation found on Windows host."
     fi
+
+    # Symlink Windows Terminal settings
+    WINTERM_USER_FOLDER=$(wslpath $(get_win_var %LOCALAPPDATA%/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/RoamingState))
+    if [ -d ${WINTERM_USER_FOLDER} ]; then 
+        echo "Symlinking Windows Terminal user settings.json..."
+        ln -sfnv "${PWD}/.wsl2/WindowsTerminal/settings.json" "${WINTERM_USER_FOLDER}/settings.json"
+    else
+        echo "No Windows Terminal installation found on Windows host."
+    fi
+
 fi
 
 
 echo "Installing dotfiles..."
 echo TODO
 #link
-
-
-
-# # shell settings
-# ln -sv ${PWD}/zsh/.zshrc ~/.zshrc
-
-# # shell settings
-# ln -sv ${PWD}/bash/.bashrc ~/.bashrc
-
-# # git configurations
-# ln -sv ${PWD}/git/.gitconfig ~/.gitconfig
-
-
-cat ${PWD}/dotfiles/vscode/extensions.txt | while read extension
-do
-    code --install-extension $extension --force
-done
