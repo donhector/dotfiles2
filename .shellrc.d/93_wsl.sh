e3876cb04f61bd9dd3a9339fc4ca80ac070a2953
Hector Molina <2475379+donhector@users.noreply.github.com>
2021-12-22 19:01:01 +0100

    updated wsl settings

--- .shellrc.d/93_wsl.sh
+++ .shellrc.d/93_wsl.sh
@@ -5,6 +5,11 @@ if [ -n "${WSL_DISTRO_NAME}" ]; then
   export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
   export LIBGL_ALWAYS_INDIRECT=1
 
+  # Some command line tools (podman login) use the location defined in XDG_RUNTIME_DIR to store some data
+  # by default in WSL the variable is not defined, so some tools might default to store the data in /tmp
+  # of they just might error out. If they error out, you might want to uncomment the following two lines
+  # export XDG_RUNTIME_DIR="${HOME}/.xdg/"
+  # [ -d "${XDG_RUNTIME_DIR}" ] || mkdir -p "${XDG_RUNTIME_DIR}"
 
   # Function to resolve Windows variables. Removes the CR windows binaries spit out
   get_win_var () { cmd.exe /c "echo ${1}" 2> /dev/null | sed -e 's/\r//g'; }
@@ -19,7 +24,7 @@ if [ -n "${WSL_DISTRO_NAME}" ]; then
     sudo service ${service} status 2>&1 >/dev/null || sudo service ${service} start
   done
 
-  # This solves issues with docker containers that rely on systemd
+  # This solves issues with docker/podman containers that rely on systemd
   [ -d "/sys/fs/cgroup/systemd" ] || sudo mkdir "/sys/fs/cgroup/systemd"
   mount | grep 'name=systemd' || sudo mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd
 
