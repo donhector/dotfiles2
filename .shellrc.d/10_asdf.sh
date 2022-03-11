c7e832d8ab85547c3a24294e775aa804973f1a42
Hector Molina <2475379+donhector@users.noreply.github.com>
2022-03-11 20:47:35 +0100

    Added logic to also auto install asdf tools

--- .shellrc.d/10_asdf.sh
+++ .shellrc.d/10_asdf.sh
@@ -1,32 +1,35 @@
 # ASDF software manager setup
 
-function install_asdf {
+install_asdf() {
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf
     cd ~/.asdf
     git checkout "$(git describe --abbrev=0 --tags)"
     cd -
 
-    # Install tools
+
+}
+
+update_asdf() {
+    asdf update
+}
+
+uninstall_asdf() {
+    rm -rf ${ASDF_DATA_DIR:-$HOME/.asdf} #~/.tool-versions
+}
+
+
+_asdf_load(){
+    if [ -f ~/.asdf/asdf.sh ]; then
+        . ~/.asdf/asdf.sh
+    fi
+}
+
+_asdf_install_tools(){
+    _asdf_load
     if [ -f ~/.tool-versions ]; then
         cut -d' ' -f1 .tool-versions | xargs -r asdf plugin add
         asdf install
     fi
 }
 
-function update_asdf {
-    asdf update
-}
-
-function uninstall_asdf {
-    rm -rf ${ASDF_DATA_DIR:-$HOME/.asdf} #~/.tool-versions
-}
-
-# Install it if not present
-if [ ! -d ~/.asdf ]; then
-    install_asdf
-fi
-
-# Load it
-if [ -f ~/.asdf/asdf.sh ]; then
-    . ~/.asdf/asdf.sh
-fi
+_asdf_load
